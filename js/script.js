58c7f5d6cd93a6a529ff37c90cf4f9ca51d4e730
Vitalii <saifutdinov_vitalii@mail.ru>
2025-05-24 16:46:02 +0500

    fdfs

--- js/script.js
+++ js/script.js
@@ -4,5 +4,21 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const header = document.querySelector('.header');
     hamburger.addEventListener('click', ()=> {
         header.classList.toggle('open')
-    })
+    });
+
+    const helpSwiper = new Swiper('.help-swiper', {
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
 })
